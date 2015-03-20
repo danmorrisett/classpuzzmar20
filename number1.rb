e5e0fc1b3074906b8899e65ef4c05c8023e9f34e
ChaseTerzian <chaseterzian@gmail.com>
2015-03-20 13:26:04 -0600

    hard code

--- number1.rb
+++ number1.rb
@@ -5,8 +5,15 @@ teams = gets.chomp
 team_names = teams.split(", ")
 
 
-puts team_names.combination().to_a
+first_team = team_names.first + " Vs. "
+second_team = team_names[1]
+third_team = team_names[2] + " Vs. "
+fourth_team = team_names[3]
 
+
+
+puts first_team + second_team
+puts third_team + fourth_team
 
 
 # def team_string(names)
